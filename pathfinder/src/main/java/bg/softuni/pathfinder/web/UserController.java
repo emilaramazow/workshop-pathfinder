0263a5b4b5f1c32c5c64e9b60a73d914448061f9
emilaramazow <emil.aramazow@gmail.com>
2022-11-01 23:20:27 +0200

    Update register.html and U serController. Added UserRegBindingModel

--- pathfinder/src/main/java/bg/softuni/pathfinder/web/UserController.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -1,10 +1,13 @@
 package bg.softuni.pathfinder.web;
 
+import bg.softuni.pathfinder.model.binding.UserRegisterBindingModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -16,7 +19,10 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String registerConfirm() {
+    public String registerConfirm(@Valid UserRegisterBindingModel userRegisterBindingModel,
+                                  BindingResult bindingResult) {
+
+        
         // TODO
 
         return "redirect:/";
