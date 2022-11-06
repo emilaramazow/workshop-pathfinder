c915d594366dec44b4304d965f269785612cde6e
emilaramazow <emil.aramazow@gmail.com>
2022-11-06 15:14:50 +0200

    Updated RouteAddBindingModel, RouteController and add-route.html

--- pathfinder/src/main/java/bg/softuni/pathfinder/web/RouteController.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/web/RouteController.java
@@ -1,22 +1,34 @@
 package bg.softuni.pathfinder.web;
 
-import bg.softuni.pathfinder.model.view.RouteViewModel;
+import bg.softuni.pathfinder.model.binding.RouteAddBindingModel;
 import bg.softuni.pathfinder.service.RouteService;
+import bg.softuni.pathfinder.util.CurrentUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/routes")
 public class RouteController {
 
     private final RouteService routeService;
+    private final CurrentUser currentUser;
 
-    public RouteController(RouteService routeService) {
+    public RouteController(RouteService routeService, CurrentUser currentUser) {
         this.routeService = routeService;
+        this.currentUser = currentUser;
+    }
+
+    @ModelAttribute
+    public RouteAddBindingModel routeAddBindingModel() {
+        return new RouteAddBindingModel();
     }
 
     @GetMapping("/all")
@@ -27,4 +39,27 @@ public class RouteController {
 
         return "routes";
     }
+
+    @GetMapping("/add")
+    public String add() {
+        if (currentUser.getId() == null) {
+            return "redirect:/users/login";
+        }
+
+        return "add-route";
+    }
+
+    @PostMapping("/add")
+    public String addConfirm(@Valid RouteAddBindingModel routeAddBindingModel,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+
+        }
+
+        return "redirect:all";
+    }
+
+
 }
