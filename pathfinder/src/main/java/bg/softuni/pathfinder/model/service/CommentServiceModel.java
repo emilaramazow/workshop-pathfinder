b3d082f6913c5e32908e2201cef895c0f41dadb0
emilaramazow <emil.aramazow@gmail.com>
2022-11-18 20:52:19 +0200

    Added integration testing

--- pathfinder/src/main/java/bg/softuni/pathfinder/model/service/CommentServiceModel.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/model/service/CommentServiceModel.java
@@ -2,15 +2,21 @@ package bg.softuni.pathfinder.model.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 public class CommentServiceModel {
 
     private Long routeId;
     private String message;
     private String creator;
 
+    public CommentServiceModel(Long routeId, String message, String creator) {
+        this.routeId = routeId;
+        this.message = message;
+        this.creator = creator;
+    }
 }
